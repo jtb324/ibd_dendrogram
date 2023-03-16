cdb13a378d572cdbbb81855935543dd58405e396
jtb324 <james.baker@vanderbilt.edu>
2023-03-16 11:36:16 -0500

    added functionality for drawing the dendrogram and updated to python 3.10

--- make_distance_matrix.py
+++ make_distance_matrix.py
@@ -1,8 +1,13 @@
+from pathlib import Path
 import pandas as pd
-from typing import List, Dict, Tuple, Callable, Any, Optional
+from typing import Callable, Any, Optional
 from numpy import zeros
+import numpy.typing as npt
+import scipy.cluster.hierarchy as sch
+from scipy.spatial.distance import squareform
+import matplotlib.pyplot as plt
 
-def check_kwargs(args_dict: Dict[str, Any]) -> Optional[str]:
+def check_kwargs(args_dict: dict[str, Any]) -> str | None:
     """Function that will make sure that the necessary arguments are passed to distance function
     
     Parameters
@@ -11,11 +16,11 @@ def check_kwargs(args_dict: Dict[str, Any]) -> Optional[str]:
         Dictionary that has the arguments as keys and the values for the distance function
     """
     if not all([elem in args_dict.keys() for elem in ["pair_1", "pair_2", "pairs_df", "cm_threshold"]]):
-        return f"Not enough arguments passed to the _determine_distances function. Expected pair_1, pair_2, pairs_df, cm_threshold"
+        return "Not enough arguments passed to the _determine_distances function. Expected pair_1, pair_2, pairs_df, cm_threshold"
 
 def _determine_distances(
     **kwargs
-) -> Tuple[Optional[str], float]:
+) -> tuple[str | None, float]:
     """Function that will determine the distances between the main grid and then each connected grid. It will use a value of 2.5 for all grids that don't share a segment. This is just the min cM value, 5cM, divided in half
 
     Parameter:
@@ -63,7 +68,7 @@ def _determine_distances(
     return None, 1 / ibd_length
 
 
-def record_matrix(output: str, matrix, pair_list: List[str]) -> None:
+def record_matrix(output: str, matrix, pair_list: list[str]) -> None:
     """Function that will write the dataframe to a file
 
     Parameters
@@ -88,11 +93,11 @@ def make_distance_matrix(
     pairs_df: pd.DataFrame,
     min_cM: int,
     distance_function: Callable = _determine_distances,
-) -> Tuple[Optional[List[str]], Any]:
+) -> tuple[list[str] | None, npt.NDArray]:
     """Function that will make the distance matrix
 
     Parameters
-
+    ----------
     pairs_df : pd.DataFrame
         dataframe that has the pairs_files. it should have at least three columns
         called 'pair_1', 'pair_2', and 'length'
@@ -102,14 +107,14 @@ def make_distance_matrix(
         get the ibd segment length when pairs do not share a segment
 
     Returns
-
+    -------
     Dict[str, Dict[str, float]]
         returns a tuple where the first object is a list of ids that has the
         individual id that corresponds to each row. The second object is the
         distance matrix
     """
     # get a list of all the unique ids in the pairs_df
-    id_list: List[str] = list(
+    id_list: list[str] = list(
         set(pairs_df.pair_1.values.tolist() + pairs_df.pair_2.values.tolist())
     )
 
@@ -137,3 +142,43 @@ def make_distance_matrix(
             
 
     return ids_index, matrix
+
+def generate_dendrogram(matrix: npt.NDArray) -> npt.NDArray:
+    """Function that will perform the hierarchical clustering algorithm
+    
+    Parameters
+    ----------
+    matrix : Array
+        distance matrix represented by 2D numpy array. distance should be calculated based on 1/(ibd segment length)
+        
+    Returns
+    -------
+    Array
+        returns the results of the clustering as a numpy array
+    """
+    squareform_matrix = squareform(matrix)
+
+    return sch.linkage(squareform_matrix, method='ward')
+
+def draw_dendrogram(clustering_results: npt.NDArray, grids: list[str], output_name: Path) -> None:
+    """Function that will draw the dendrogram"""
+    plt.figure(figsize=(15, 12))
+    ax = plt.subplot(111)
+
+    # Temporarily override the default line width:
+    with plt.rc_context({'lines.linewidth': 2}):
+        dendrogram = sch.dendrogram(
+            clustering_results, 
+            labels=grids,
+            orientation="left",
+            color_threshold=0,
+            above_threshold_color="black"
+            )
+
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    ax.axes.get_xaxis().set_visible(False)
+
+    plt.savefig(output_name)
